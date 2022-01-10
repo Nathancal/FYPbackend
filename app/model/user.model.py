efab544ff364e769007649a2e121569430e8e758
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2022-01-10 19:25:41 +0000

    update

---
+++ app/model/user.model.py
@@ -0,0 +1,12 @@
+from mongoengine.document import Document
+from mongoengine.fields import BooleanField, EmailField, StringField
+
+
+class User(Document):
+        
+        userID = StringField(required=True)
+        email = EmailField(required=True, unique=True)
+        firstName = StringField(max_length=40, required=True)
+        lastName = StringField(max_length=40, required=True)
+        password = StringField(required=True)
+        admin = BooleanField(default=False)
